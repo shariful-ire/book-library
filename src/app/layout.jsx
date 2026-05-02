aa442813ee65ea19de4674321e8b53a4a7ce4fd9
shariful-ire <shariful.ire@gmail.com>
2026-05-03 04:15:58 +0600

    fix the app/layout.jsx file

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,14 @@
 import "./globals.css";
+import AuthProvider from "@/providers/AuthProvider";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <AuthProvider>
+          {children}
+        </AuthProvider>
+      </body>
     </html>
   );
 }
